1d9d761ff98e58be94b15e4aefa490f2f5134bff
AJINKYA <ajinkyacodes@gmail.com>
2025-07-31 12:20:37 +0530

    datatypes assignment solved

---
+++ assignments/datatypes.py
@@ -0,0 +1,20 @@
+# Person Information (Data)
+firstName = "Peter"
+lastName = "Parker"
+age = 25
+ssn = "100-00-00"
+height = 6.1
+weight = 64.25
+
+print("Driver License Application")
+print(firstName, lastName, age, ssn, height, weight)
+
+print("\nDriver License Application (Different Approach)")
+print(firstName + " " + lastName + ", " + str(age) + ", " + ssn + ", " + str(height) + ", " + str(weight))
+
+print("\nDriver License Application (Standard Approach)")
+print("Name: " + firstName + " " + lastName)
+print("Age: " + str(age))
+print("SSN: " + ssn)
+print("Height: " + str(height))
+print("Weight: " + str(weight))
