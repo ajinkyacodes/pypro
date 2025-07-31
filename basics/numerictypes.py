8c502ad3f0c3bf8a320055953e0b0e2778233960
AJINKYA <ajinkyacodes@gmail.com>
2025-07-31 11:59:57 +0530

    numerictypes code added

---
+++ basics/numerictypes.py
@@ -0,0 +1,52 @@
+userID = 123
+userid = 123 # case sensitive
+user_id = 123
+
+# Integers
+a=13
+b=100
+c=-66
+print(a,b,c)
+print(type(a))
+
+# Floats
+x=33.5
+y=-25.8
+z=205.0
+print(x,y,z)
+print(type(z))
+
+# Complex
+d=3+5j
+print(d)
+print(type(d))
+
+# Binary
+e=0B1010
+print(e)
+print(type(e))
+
+# Hex
+f=0XFF
+print(f)
+print(type(f))
+
+# Boolean
+g=True
+print(type(g))
+print(9>8)
+
+# Type Conversion
+h=int(x)
+print(h)
+print(type(h))
+
+# String to float
+i=float("22.5")
+print(i)
+print(type(i))
+
+# Binary, Hex, Octal
+print(bin(10))
+print(hex(10))
+print(oct(10))
