1aaf21f84439707fbe90d5ff4955df38f88c4fec
AJINKYA <ajinkyacodes@gmail.com>
2025-07-31 12:41:07 +0530

    string slicing and reverse string code added

--- basics/stringtype.py
+++ basics/stringtype.py
@@ -16,3 +16,16 @@ print(s * 2)
 
 # Length of string
 print(len(s))
+
+# Slicing of string
+print(s[0:5])
+print(s[0:])
+print(s[:8])
+print(s[-3:-1])
+
+# Slicing of string with steps
+print(s[0:9:2])
+
+# Reverse the string
+print(s[15::-1])
+print(s[::-1])
