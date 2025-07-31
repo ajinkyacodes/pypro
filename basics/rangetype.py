8e717f0a46d78b760f5768789d511f494c209bab
AJINKYA <ajinkyacodes@gmail.com>
2025-07-31 18:44:59 +0530

    rangetype code added

---
+++ basics/rangetype.py
@@ -0,0 +1,40 @@
+r=range(5)
+for i in r:
+    print(i)
+
+"""
+OUTPUT:
+0
+1
+2
+3
+4
+"""
+
+print("\n\n")
+r=range(1,6)
+for j in r:
+    print(j)
+
+"""
+OUTPUT:
+1
+2
+3
+4
+5
+"""
+
+print("\n\n")
+r=range(1,15,3)
+for k in r:
+    print(k)
+
+"""
+OUTPUT:
+1
+4
+7
+10
+13
+"""
