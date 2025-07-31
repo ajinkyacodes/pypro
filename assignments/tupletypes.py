8cf920a301d55406340ba7d4a319bed94461d725
AJINKYA <ajinkyacodes@gmail.com>
2025-07-31 18:37:49 +0530

    tupletypes assignment solved

---
+++ assignments/tupletypes.py
@@ -0,0 +1,11 @@
+my_tuple = (1, 2, 3, "x", "y", "z")
+
+# Print first and third elements
+print(my_tuple[1], my_tuple[3])  # 2 x
+
+# Print the length
+print(len(my_tuple))  # 6
+
+# Check if the element exists
+print("a" in my_tuple)  # False
+print("x" in my_tuple)  # True
