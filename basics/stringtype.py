e891c3257e7486afe29f5185f6512b5b926ffbd3
AJINKYA <ajinkyacodes@gmail.com>
2025-07-31 12:31:14 +0530

    stringtype code added

---
+++ basics/stringtype.py
@@ -0,0 +1,18 @@
+# Single line string
+s = "You are awesome"
+print(s)
+
+# Multiline string
+s1 = """You are
+the creator
+of your destiny."""
+print(s1)
+
+# Printing the character of the string
+print(s[0])
+
+# Repeat string multiple times
+print(s * 2)
+
+# Length of string
+print(len(s))
