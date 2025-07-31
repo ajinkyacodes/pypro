e3957c222749b83e9141eaa8e96e0db740baf846
AJINKYA <ajinkyacodes@gmail.com>
2025-07-31 19:30:21 +0530

    datatypes case study of Patient usecase code added

---
+++ basics/datatypes.py
@@ -0,0 +1,9 @@
+id = 1
+firstName = "John"
+lastName = "      Baily"
+ssn = "123-45-6789"
+hasInsurance = True
+billingAmount = "10000"
+billingAmount = float(billingAmount)
+print(type(billingAmount))
+print(id, firstName, lastName.lstrip(), ssn, hasInsurance, billingAmount, ssn[7:len(ssn)])
